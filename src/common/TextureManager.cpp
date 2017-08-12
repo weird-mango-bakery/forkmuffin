eebf157b6ef1f958aee263900b5828a9113a8dc1
Kracav4ik <dikama2013@yandex.ru>
2017-08-12 03:16:48 +0300

    documentation for texture manager

--- src/common/TextureManager.cpp
+++ src/common/TextureManager.cpp
@@ -3,27 +3,35 @@
 #include <QCoreApplication>
 
 const QImage& TextureManager::getImage(const QString& name) {
-    QString path = QString("%1/../data/textures/%2").arg(QCoreApplication::applicationDirPath()).arg(name);
+    QString path = QString("%1/%2").arg(getTextureDir()).arg(name);
     if (imgs.contains(path)) {
         return imgs[path];
     }
     QImage img(path);
     if (img.isNull()) {
-        return emptyImage;
+        return getInvalid();
     }
     imgs[path] = img;
     return imgs[path];
 }
 
 TextureManager::TextureManager() {
-    emptyImage = QImage(2, 2, QImage::Format_RGB32);
-    emptyImage.setPixel(0, 0, 0xff00ff);
-    emptyImage.setPixel(1, 0, 0xffff00);
-    emptyImage.setPixel(0, 1, 0x00ff00);
-    emptyImage.setPixel(1, 1, 0x0080ff);
+    invalidImage = QImage(2, 2, QImage::Format_RGB32);
+    invalidImage.setPixel(0, 0, 0xff00ff);
+    invalidImage.setPixel(1, 0, 0xffff00);
+    invalidImage.setPixel(0, 1, 0x00ff00);
+    invalidImage.setPixel(1, 1, 0x0080ff);
 }
 
 TextureManager& TextureManager::get() {
     static TextureManager manager;
     return manager;
+}
+
+const QImage& TextureManager::getInvalid() const {
+    return invalidImage;
+}
+
+QString TextureManager::getTextureDir() {
+    return QString("%1/../data/textures").arg(QCoreApplication::applicationDirPath());
 }
--- src/common/TextureManager.h
+++ src/common/TextureManager.h
@@ -4,13 +4,24 @@
 #include <QString>
 #include <QHash>
 
+//! Class that contains and caches textures.
 class TextureManager {
 private:
+    //! Map of texture name to texture image.
     QHash<QString, QImage> imgs;
-    QImage emptyImage;
+    //! Invalid image to return if the texture was not loaded.
+    QImage invalidImage;
+    //! Private constructor so the texture manager can be created inside get() only.
     TextureManager();
 
 public:
+    //! Returns the invalid image.
+    const QImage& getInvalid() const;
+    //! Returns texture from cache or load it from texture name.
+    //! \param s texture name.
     const QImage& getImage(const QString& s);
+    //! The static function to get or create the texture manager.
     static TextureManager& get();
+    //! Returns path to directory with textures.
+    static QString getTextureDir();
 };
