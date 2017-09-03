7fd5816a56c4ab5795a9ddfe9ed03b6753763491
Kracav4ik <dikama2013@yandex.ru>
2017-09-03 14:12:18 +0300

    Improved logic to merge together similar commands

--- src/editor/commands/ChangeStrayImageParamsCommand.cpp
+++ src/editor/commands/ChangeStrayImageParamsCommand.cpp
@@ -2,6 +2,9 @@
 #include "editor/EditorMainWindow.h"
 #include "common/StrayImage.h"
 
+ChangeStrayImageParamsCommand::CommandType ChangeStrayImageParamsCommand::lastCommandType = CommandType::SET_TEXTURE;
+int ChangeStrayImageParamsCommand::lastCommandId = 1;
+
 void ChangeStrayImageParamsCommand::undo() {
     currentImage.setPos(oldPos);
     currentImage.setSize(oldSize);
@@ -15,8 +18,15 @@ void ChangeStrayImageParamsCommand::redo() {
 }
 
 ChangeStrayImageParamsCommand::ChangeStrayImageParamsCommand(
-    EditorMainWindow& editor, StrayImage& currentImage, const QPoint& pos, const QSize& size, const QString& textureName)
-    : EditorCommand(editor, "Change image params")
+    EditorMainWindow& editor,
+    const QString& text,
+    StrayImage& currentImage,
+    const QPoint& pos,
+    const QSize& size,
+    const QString& textureName,
+    CommandType type
+)
+    : EditorCommand(editor, text)
     , currentImage(currentImage)
     , pos(pos)
     , size(size)
@@ -24,24 +34,106 @@ ChangeStrayImageParamsCommand::ChangeStrayImageParamsCommand(
     , oldPos(currentImage.getPos())
     , oldSize(currentImage.getSize())
     , oldTextureName(currentImage.getTexture())
-{}
+    , commandType(type)
+    , commandId(type == lastCommandType ? lastCommandId : ++lastCommandId)
+{
+    lastCommandType = type;
+}
 
 EditorCommand* ChangeStrayImageParamsCommand::setX(EditorMainWindow& editor, StrayImage& currentImage, int newX) {
-    return new ChangeStrayImageParamsCommand(editor, currentImage, QPoint(newX, currentImage.getPos().y()), currentImage.getSize(), currentImage.getTexture());
+    return new ChangeStrayImageParamsCommand(
+        editor,
+        "Change x",
+        currentImage,
+        QPoint(newX, currentImage.getPos().y()),
+        currentImage.getSize(),
+        currentImage.getTexture(),
+        CommandType::SET_X
+    );
 }
 
 EditorCommand* ChangeStrayImageParamsCommand::setY(EditorMainWindow& editor, StrayImage& currentImage, int newY) {
-    return new ChangeStrayImageParamsCommand(editor, currentImage, QPoint(currentImage.getPos().x(), newY), currentImage.getSize(), currentImage.getTexture());
+    return new ChangeStrayImageParamsCommand(
+        editor,
+        "Change y",
+        currentImage,
+        QPoint(currentImage.getPos().x(), newY),
+        currentImage.getSize(),
+        currentImage.getTexture(),
+        CommandType::SET_Y
+    );
 }
 
 EditorCommand* ChangeStrayImageParamsCommand::setWidth(EditorMainWindow& editor, StrayImage& currentImage, int newWidth) {
-    return new ChangeStrayImageParamsCommand(editor, currentImage, currentImage.getPos(), QSize(newWidth, currentImage.getSize().height()), currentImage.getTexture());
+    return new ChangeStrayImageParamsCommand(
+        editor,
+        "Change width",
+        currentImage,
+        currentImage.getPos(),
+        QSize(newWidth, currentImage.getSize().height()),
+        currentImage.getTexture(),
+        CommandType::SET_WIDTH
+    );
 }
 
 EditorCommand* ChangeStrayImageParamsCommand::setHeight(EditorMainWindow& editor, StrayImage& currentImage, int newHeight) {
-    return new ChangeStrayImageParamsCommand(editor, currentImage, currentImage.getPos(), QSize(currentImage.getSize().width(), newHeight), currentImage.getTexture());
+    return new ChangeStrayImageParamsCommand(
+        editor,
+        "Change height",
+        currentImage,
+        currentImage.getPos(),
+        QSize(currentImage.getSize().width(), newHeight),
+        currentImage.getTexture(),
+        CommandType::SET_HEIGHT
+    );
 }
 
 EditorCommand* ChangeStrayImageParamsCommand::setTexture(EditorMainWindow& editor, StrayImage& currentImage, const QString& newTex) {
-    return new ChangeStrayImageParamsCommand(editor, currentImage, currentImage.getPos(), currentImage.getSize(), newTex);
+    return new ChangeStrayImageParamsCommand(
+        editor,
+        "Change texture",
+        currentImage,
+        currentImage.getPos(),
+        currentImage.getSize(),
+        newTex,
+        CommandType::SET_TEXTURE
+    );
+}
+
+int ChangeStrayImageParamsCommand::id() const {
+    return commandId;
+}
+//! The macro generates a case block that checks if the difference between new and old properties are the same
+//! for two commands. If they are, the current command gets new value from the other command, and true is returned.
+//! \param name all-lowercase name of the getter of the property.
+//! \param Name capitalised name for setter of the property.
+//! \param NAME all-uppercase name for command type enum value.
+//! \param var field in the command that contains the new values.
+//! \param oldVar field in the command that contains the old values.
+#define MERGE(name, Name, NAME, var, oldVar)                                                        \
+case CommandType::SET_##NAME:                                                                       \
+    if ((var.name() - oldVar.name() < 0) != (command->var.name() - command->oldVar.name() < 0)) {   \
+        return false;                                                                               \
+    }                                                                                               \
+    var.set##Name(command->var.name());                                                             \
+    break;
+
+//! The macro checks if the difference between new and old coordinates are the same for two commands, see MERGE().
+//! Since coordinate name consists only of one letter, its capitalized and all-uppercase names are the same.
+#define MERGE_POS(name, NAME) MERGE(name, NAME, NAME, pos, oldPos)
+
+//! The macro checks if the difference between new and old width or height are the same for two commands, see MERGE().
+#define MERGE_SIZE(name, Name, NAME) MERGE(name, Name, NAME, size, oldSize)
+
+bool ChangeStrayImageParamsCommand::mergeWith(const QUndoCommand* other) {
+    auto command = static_cast<const ChangeStrayImageParamsCommand*>(other);
+    switch(commandType) {
+        MERGE_POS(x, X)
+        MERGE_POS(y, Y)
+        MERGE_SIZE(width, Width, WIDTH)
+        MERGE_SIZE(height, Height, HEIGHT)
+        default:
+            return false;
+    }
+    return true;
 }
--- src/editor/commands/ChangeStrayImageParamsCommand.h
+++ src/editor/commands/ChangeStrayImageParamsCommand.h
@@ -7,7 +7,8 @@
 class EditorMainWindow;
 class StrayImage;
 
-//! A command for changing stray image params.
+//! A command for changing stray image params. Each command can change only one of the following params:
+//! x, y, width, height or texture name.
 class ChangeStrayImageParamsCommand : public EditorCommand {
 private:
     //! The reference to stray image that we are changing.
@@ -25,9 +26,49 @@ private:
     //! The current texture name of the image.
     QString oldTextureName;
 
+    //! The enum class that describes which one of the params the command changes.
+    enum class CommandType {
+        //! The command changes x coordinates.
+        SET_X,
+        //! The command changes y coordinates.
+        SET_Y,
+        //! The command changes width.
+        SET_WIDTH,
+        //! The command changes height.
+        SET_HEIGHT,
+        //! The command changes texture name.
+        SET_TEXTURE,
+    };
+    //! The type of the command.
+    CommandType commandType;
+    //! The type of the last created command.
+    static CommandType lastCommandType;
+    //! The unique id for merge, see mergeWith().
+    int commandId;
+    //! The id that was used when the last command had been created.
+    static int lastCommandId;
+
     //! Creates the command.
     ChangeStrayImageParamsCommand(
-        EditorMainWindow& editor, StrayImage& currentImage, const QPoint& pos, const QSize& size, const QString& textureName);
+        EditorMainWindow& editor,
+        const QString& text,
+        StrayImage& currentImage,
+        const QPoint& pos,
+        const QSize& size,
+        const QString& textureName,
+        CommandType type
+    );
+    //! Returns the command id. Qt tries to merge commands with the same id by calling mergeWith().
+    //! Commands with the same type, that are created one after another, have the same id.
+    //! When a command with a different type is created it gets a new unique id.
+    int id() const override;
+    //! Returns true if the command successfully merges with the specified command.
+    //! For command that changes position (either x or y), only changes in the same direction are merged.
+    //! For command that changes size (either width or height), value increments are merged only
+    //! with another increments, same for decrements.
+    //! \param other the new command to try merging with.
+    bool mergeWith(const QUndoCommand* other) override;
+
 public:
     //! Creates the command that changes only x.
     static EditorCommand* setX(EditorMainWindow& editor, StrayImage& currentImage, int newX);
